b58203dab1d1bfb79a58d6715695cc15ed143859
Andrew MacIsaac <amacisaac95@gmail.com>
2023-03-20 22:14:58 -0600

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+    const form = document.getElementById('create-task-form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const commentText = document.getElementById('new-task-description');
+      const comment = document.createElement("li");
+      comment.textContent = commentText.value;
+      form.appendChild(comment);
+
+      event.target.reset();
+    });
+  
 });
